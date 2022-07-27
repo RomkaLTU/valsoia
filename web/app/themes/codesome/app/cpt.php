e4dbee8f65e6df920fc5de16bc43c493c6074654
RD <hello@codesomelabs.com>
2022-07-27 22:58:21 +0300

    Taxonomy, filters, from BE

--- web/app/themes/codesome/app/cpt.php
+++ web/app/themes/codesome/app/cpt.php
@@ -1,44 +1,6 @@
 <?php
 
-function cptui_register_my_taxes(): void
-{
-
-    /**
-     * Taxonomy: Prod. categories.
-     */
-
-    $labels = [
-        "name" => __("Prod. categories", "codesome"),
-        "singular_name" => __("Prod. category", "codesome"),
-    ];
-
-
-    $args = [
-        "label" => __("Prod. categories", "codesome"),
-        "labels" => $labels,
-        "public" => true,
-        "publicly_queryable" => true,
-        "hierarchical" => false,
-        "show_ui" => true,
-        "show_in_menu" => true,
-        "show_in_nav_menus" => true,
-        "query_var" => true,
-        "rewrite" => [ 'slug' => 'product_categories', 'with_front' => true, ],
-        "show_admin_column" => true,
-        "show_in_rest" => true,
-        "show_tagcloud" => true,
-        "rest_base" => "product_categories",
-        "rest_controller_class" => "WP_REST_Terms_Controller",
-        "rest_namespace" => "wp/v2",
-        "show_in_quick_edit" => true,
-        "sort" => true,
-        "show_in_graphql" => false,
-    ];
-    register_taxonomy("product_categories", [ "products" ], $args);
-}
-add_action('init', 'cptui_register_my_taxes');
-
-function cptui_register_my_cpts(): void
+function cptui_register_my_cpts()
 {
 
     /**
@@ -82,3 +44,74 @@ function cptui_register_my_cpts(): void
 }
 
 add_action('init', 'cptui_register_my_cpts');
+
+function cptui_register_my_taxes()
+{
+
+    /**
+     * Taxonomy: Prod. categories.
+     */
+
+    $labels = [
+        "name" => __("Prod. categories", "codesome"),
+        "singular_name" => __("Prod. category", "codesome"),
+    ];
+
+
+    $args = [
+        "label" => __("Prod. categories", "codesome"),
+        "labels" => $labels,
+        "public" => true,
+        "publicly_queryable" => true,
+        "hierarchical" => true,
+        "show_ui" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "query_var" => true,
+        "rewrite" => [ 'slug' => 'product-categories', 'with_front' => true,  'hierarchical' => true, ],
+        "show_admin_column" => true,
+        "show_in_rest" => true,
+        "show_tagcloud" => true,
+        "rest_base" => "product_categories",
+        "rest_controller_class" => "WP_REST_Terms_Controller",
+        "rest_namespace" => "wp/v2",
+        "show_in_quick_edit" => true,
+        "sort" => true,
+        "show_in_graphql" => false,
+    ];
+    register_taxonomy("product_categories", [ "products" ], $args);
+
+    /**
+     * Taxonomy: Options.
+     */
+
+    $labels = [
+        "name" => __("Options", "codesome"),
+        "singular_name" => __("Option", "codesome"),
+    ];
+
+
+    $args = [
+        "label" => __("Options", "codesome"),
+        "labels" => $labels,
+        "public" => true,
+        "publicly_queryable" => true,
+        "hierarchical" => false,
+        "show_ui" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "query_var" => true,
+        "rewrite" => [ 'slug' => 'options', 'with_front' => true,  'hierarchical' => true, ],
+        "show_admin_column" => true,
+        "show_in_rest" => true,
+        "show_tagcloud" => true,
+        "rest_base" => "product_options",
+        "rest_controller_class" => "WP_REST_Terms_Controller",
+        "rest_namespace" => "wp/v2",
+        "show_in_quick_edit" => true,
+        "sort" => true,
+        "show_in_graphql" => false,
+    ];
+    register_taxonomy("product_options", [ "products" ], $args);
+}
+add_action('init', 'cptui_register_my_taxes');
