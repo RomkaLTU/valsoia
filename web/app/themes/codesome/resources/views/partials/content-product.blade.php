171f25770b53b865fe27f0a252a2c3a0ed4ec4c7
RD <hello@codesomelabs.com>
2022-07-27 23:03:12 +0300

    fix image height

--- web/app/themes/codesome/resources/views/partials/content-product.blade.php
+++ web/app/themes/codesome/resources/views/partials/content-product.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ get_the_permalink() }}" class="">
-  @php(the_post_thumbnail('large', ['class' => 'h-[279px] 2xl:h-[379px] w-full object-cover shadow-lg']))
+  @php(the_post_thumbnail('large', ['class' => 'h-[279px] w-full object-cover shadow-lg']))
   <h2 class="text-center font-ga text-3xl mt-4">@php(the_title())</h2>
 </a>
