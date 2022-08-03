344b3ea7d42363b8e057e938c9a68a2a1f3367c9
RD <hello@codesomelabs.com>
2022-08-03 14:17:35 +0300

    Related products bg fixes

--- web/app/themes/codesome/app/View/Composers/FrontPage.php
+++ web/app/themes/codesome/app/View/Composers/FrontPage.php
@@ -14,7 +14,6 @@ class FrontPage extends Composer
     {
         return [
             'products' => $this->getProducts(),
-            'instagram' => $this->getInstaFeed(),
         ];
     }
 
@@ -22,14 +21,8 @@ class FrontPage extends Composer
     {
         return get_posts([
             'post_type' => 'products',
+            'post_status' => 'publish',
             'posts_per_page' => 10,
         ]);
-    }
-
-    public function getInstaFeed(): array
-    {
-        // Get the instagram feed
-
-        return [];
     }
 }
