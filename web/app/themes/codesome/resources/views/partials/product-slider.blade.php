8c27b7ba65483f4af5f6f158a10d1443b69a5583
RD <hello@codesomelabs.com>
2022-08-19 14:38:00 +0300

    Fix PHP tag blade issue.

--- web/app/themes/codesome/resources/views/partials/product-slider.blade.php
+++ web/app/themes/codesome/resources/views/partials/product-slider.blade.php
@@ -1,10 +1,10 @@
 <div class="swiper-products overflow-hidden relative sm:w-[95%] max-w-[1557px] mx-auto sm:px-4 py-6">
   <div class="swiper-wrapper items-baseline font-ga text-3xl">
     @foreach($products as $product)
-      @php
+      <?php
         $originalTaxonomyId = apply_filters('wpml_object_id', \Illuminate\Support\Arr::first(get_the_terms($product->id, 'product_categories'))->term_id, 'product_categories', false, 'en');
         $transparentProdImage = get_field('transparent_image_bg', 'term_' . $originalTaxonomyId);
-      @endphp
+      ?>
       <a href="{{ get_permalink($product) }}" class="swiper-slide group flex flex-col gap-6 items-center">
         @if($transparentProdImage)
           {!! get_the_post_thumbnail($product, 'large', ['class' => 'w-full h-full object-contain group-hover:-translate-y-6 transition-transform duration-1000']) !!}
