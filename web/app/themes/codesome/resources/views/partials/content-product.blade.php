f57ef3b34d6dd7786a30d4ff1178759ec30d97b3
RD <hello@codesomelabs.com>
2022-08-19 13:30:43 +0300

    Product image hovers

--- web/app/themes/codesome/resources/views/partials/content-product.blade.php
+++ web/app/themes/codesome/resources/views/partials/content-product.blade.php
@@ -1,8 +1,8 @@
-<a href="{{ get_the_permalink() }}" class="">
+<a href="{{ get_the_permalink() }}" class="group">
   @if($transparentProdImage)
-    @php(the_post_thumbnail('large', ['class' => 'w-full']))
+    @php(the_post_thumbnail('large', ['class' => 'w-full group-hover:-translate-y-6 transition-transform duration-1000']))
   @else
-    @php(the_post_thumbnail('large', ['class' => 'w-full shadow-lg']))
+    @php(the_post_thumbnail('large', ['class' => 'w-full shadow-lg group-hover:-translate-y-6 transition-transform duration-1000']))
   @endif
   <h2 class="text-center font-ga text-3xl mt-4">@php(the_title())</h2>
 </a>
--- web/app/themes/codesome/resources/views/partials/product-slider.blade.php
+++ web/app/themes/codesome/resources/views/partials/product-slider.blade.php
@@ -1,8 +1,8 @@
-<div class="swiper-products overflow-hidden relative sm:w-[95%] max-w-[1557px] mx-auto sm:px-4">
+<div class="swiper-products overflow-hidden relative sm:w-[95%] max-w-[1557px] mx-auto sm:px-4 py-6">
   <div class="swiper-wrapper items-baseline font-ga text-3xl">
     @foreach($products as $product)
-      <a href="{{ get_permalink($product) }}" class="swiper-slide flex flex-col gap-6 items-center">
-        {!! get_the_post_thumbnail($product, 'large', ['class' => 'w-full h-full object-contain']) !!}
+      <a href="{{ get_permalink($product) }}" class="swiper-slide group flex flex-col gap-6 items-center">
+        {!! get_the_post_thumbnail($product, 'large', ['class' => 'w-full h-full object-contain group-hover:-translate-y-6 transition-transform duration-1000']) !!}
         <h3 class="text-center">{{ $product->post_title }}</h3>
       </a>
     @endforeach
