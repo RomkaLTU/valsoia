06be4d2e9c2ee4669c9702e9e4c9cd8f78cbb653
RD <hello@codesomelabs.com>
2022-10-07 09:44:18 +0300

    Updates and requested changes for frontpage.

---
+++ web/app/plugins/sitepress-multilingual-cms/classes/menu-elements/PostLinkUrl.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WPML\TM\Menu;
+
+use WPML\FP\Fns;
+use WPML\FP\Lst;
+use WPML\FP\Maybe;
+use WPML\FP\Obj;
+use WPML\LIB\WP\Post;
+use function WPML\FP\pipe;
+
+class PostLinkUrl {
+	/**
+	 * @param int $postId
+	 *
+	 * @return string
+	 */
+	public function viewLinkUrl( $postId ) {
+		return Maybe::of( $postId )
+		            ->map( Post::get() )
+		            ->reject( pipe( Obj::prop( 'post_status' ), Lst::includes( Fns::__, [
+			            'private',
+			            'trash',
+		            ] ) ) )
+		            ->map( 'get_permalink' )
+		            ->getOrElse( '' );
+	}
+}
